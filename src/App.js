2d5ee5ae3317bbb6913ed746870ecdc261316cae
boqingren <boqingre@hotmail.com>
2018-06-30 19:02:17 +0800

    在项目中暴露 (eject) Webpack 文件，并在项目中配置 less 的使用

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <p className="test-less">在项目中暴露 (eject) Webpack 文件，并在项目中配置 less 的使用。</p>
         </p>
       </div>
     );
